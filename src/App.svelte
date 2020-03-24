41405570256667be75894677d7d88f1638eeb904
sooterbw <sooterbw@gmail.com>
2020-03-23 22:50:26 -0500

    Created homepage layout and started particle canvas.

---
+++ src/App.svelte
@@ -0,0 +1,71 @@
+<script>
+	import Nav from "./templates/Nav.svelte";
+	import { onMount } from "svelte";
+	export let name;
+
+	let canvas;
+
+	onMount(() => {
+		canvas.width = window.innerWidth;
+		canvas.height = window.innerHeight;
+		const ctx = canvas.getContext('2d');
+		ctx.beginPath();
+		ctx.arc(100, 100, 2, 0, 2 * Math.PI);
+		ctx.fillStyle = "#5e5e5e";
+		ctx.fill();
+		// mapCanvas(50)
+	})
+
+	function mapCanvas(count) {
+		const ctx = canvas.getContext('2d');
+		for(let i = 0; i <= count; i++) {
+			ctx.beginPath();
+			ctx.arc(
+				Math.round(Math.random() * window.innerWidth), 
+				Math.round(Math.random() * window.innerHeight), 
+				2, 0, 2 * Math.PI
+				);
+			ctx.fillStyle = "#5e5e5e";
+			ctx.fill();
+		}
+	}
+
+	function animateCanvas() {
+		requestAnimationFrame(animate)
+	}
+</script>
+
+<main>
+	<canvas bind:this={canvas}></canvas>
+	<h1>{name}</h1>
+	<Nav />
+</main>
+
+<style>
+	main {
+		background-color: #141414;
+		height: 100vh;
+		text-align: center;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	canvas {
+		position: absolute;
+		top: 0;
+		left: 0;
+	}
+
+	h1 {
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 700;
+		margin: 0;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
---
+++ src/templates/Nav.svelte
@@ -0,0 +1,32 @@
+<div id="navContainer">
+    <div id="navContents">
+        <div><a href="">Home</a></div>
+        <div><a href="">Portfolio</a></div>
+        <div><a href="">About</a></div>
+        <div><a href="">Contact</a></div>
+    </div>
+    <hr>
+</div>
+
+<style>
+    #navContainer {
+        position: absolute;
+        width: 33%;
+        bottom: 2em;
+        right: 0;
+    }
+
+    #navContents {
+        display: flex;
+        padding-left: 0.5em;
+    }
+
+        #navContents div {
+            padding-right: 1em;
+            font-weight: bold;
+        }
+
+    hr {
+
+    }
+</style>
